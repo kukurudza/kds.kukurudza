a55151cdb2f6c2c0d50e6506684cbed1d0b37066
KDS <d.kukurudza.23@gmail.com>
2021-06-04 15:05:54 +0300

    initial version

---
+++ app/dist/js/grid.js
@@ -0,0 +1,25 @@
+module.exports = {
+    outputStyle: 'scss',
+    columns: 12,
+    offset: '30px', /* gutter width px || % || rem */
+    mobileFirst: true, /* mobileFirst ? 'min-width' : 'max-width' */
+    container: {
+        maxWidth: '1200px', /* max-width оn very large screen */
+        fields: '30px' /* side fields */
+    },
+    breakPoints: {
+        lg: {
+            width: '1100px', /* -> @media (max-width: 1100px) */
+        },
+        md: {
+            width: '1024px'
+        },
+        sm: {
+            width: '768px',
+            fields: '15px' /* set fields only if you want to change container.fields */
+        },
+        xs: {
+            width: '320px'
+        }       
+    }
+}
---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const { prependListener } = require('gulp');
+
+sass.compiler = require('node-sass');
+// ---------------BrowserSync------------------------------
+gulp.task('browser-sync', function(done){
+    browserSync.init({
+        server: {
+            baseDir: './app/dist'
+        },
+        notify: false
+    });
+    browserSync.watch('app/dist/index.html').on('change', browserSync.reload);
+    done()
+});
+//-----------------Autoprefixer----------------------------
+gulp.task('autoprefixer', function(done){
+    gulp.src('app/dist/css/main.css')
+    .pipe(autoprefixer({
+        cascade: false
+    }))
+    .pipe(gulp.dest('app/dist/css'))
+    done()
+});
+//-----------------Sass-compiler----------------------------
+gulp.task('sass', function(){
+    return gulp.src('app/src/scss/**/*.*')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/dist/css'))
+    .pipe(autoprefixer())
+    .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('default', gulp.series('sass', 'browser-sync', 'autoprefixer', function(done){
+    gulp.watch('app/src/scss/*.*', gulp.series('sass'));
+    done()
+}));
